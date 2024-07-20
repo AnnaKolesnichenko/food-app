8be677a17f11e51cc8080209fbffed6de567e54d
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2024-07-20 18:38:01 +0300

    edit

---
+++ src/componets/MainPage/SubscriptionArea/SubscriptionArea.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const SubscrContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+export const SubscrForm = styled.form``;
+
+export const SubscrInput = styled.input``;
+
+export const SubscrBtn = styled.button``;
